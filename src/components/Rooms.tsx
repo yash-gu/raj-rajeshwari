25eacfd05436ace6b2ec854e2cccbc823d6f8d9c
yash-gu28 <s24cseu2221@bennett.edu.in>
2026-06-12 18:46:37 +0530

    y

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -21,7 +21,7 @@ import {
 import room1 from '../assets/images/gallery-1.jpg';
 import room2 from '../assets/images/gallery-2.jpg';
 import room3 from '../assets/images/gallery-3.jpg';
-import standardRoom from '../assets/images/standard-double-room.avif';
+import standardRoom from '../assets/images/standard-double-room.jpg';
 import queenRoom from '../assets/images/queen-room.jpeg';
 import kingRoom from '../assets/images/king-room.jpg';
 
