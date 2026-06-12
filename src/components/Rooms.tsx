0f02afb9244d1e3fbcd5bb4ffb2cde2709739142
yash-gu28 <s24cseu2221@bennett.edu.in>
2026-06-12 18:23:52 +0530

    y

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -23,7 +23,7 @@ import room2 from '../assets/images/gallery-2.jpg';
 import room3 from '../assets/images/gallery-3.jpg';
 import standardRoom from '../assets/images/standard-double-room.avif';
 import queenRoom from '../assets/images/queen-room.jpeg';
-import kingRoom from '../assets/images/king-room.avif';
+import kingRoom from '../assets/images/king-room.jpg';
 
 const rooms = [
   {
